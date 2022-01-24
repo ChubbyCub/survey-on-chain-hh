cc845c8184d26ed8acbcb9d9c2114b1d8ac5365c
Elise Pham <36690142+ChubbyCub@users.noreply.github.com>
2022-01-23 21:03:01 -0800

    config for etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,10 +41,18 @@ const config: HardhatUserConfig = {
       url: "http://localhost:8545",
       allowUnlimitedContractSize: true,
     },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
