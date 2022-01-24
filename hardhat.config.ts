e48057e151cc3b03c10c9af0a0833fee2744f2f8
Elise Pham <36690142+ChubbyCub@users.noreply.github.com>
2022-01-23 20:23:28 -0800

    hardhat files

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+  },
+  networks: {
+    testnet: {
+      url: `https://api.s0.b.hmny.io`,
+      accounts: [`0x${process.env.HARMONY_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://api.harmony.one`,
+      accounts: [`0x${process.env.HARMONY_PRIVATE_KEY}`],
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      allowUnlimitedContractSize: true,
+    },
+    hardhat: {
+      chainId: 1337,
+      allowUnlimitedContractSize: true,
+    },
+  },
+};
+
+export default config;
